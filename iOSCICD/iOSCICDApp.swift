acad8ed069529112cf21196b705b865ab2074362
Luke Bresler <luke@realpixel.co.za>
2026-02-03 08:48:01 +0200

    Initial Commit

---
+++ iOSCICD/iOSCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCICDApp.swift
+//  iOSCICD
+//
+//  Created by Luke Bresler on 2026/02/03.
+//
+
+import SwiftUI
+
+@main
+struct iOSCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
